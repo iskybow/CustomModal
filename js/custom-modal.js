9b84bc6e28fdffc621701ac10f8f966a7d05d870
@boyko <kir_boyko@bk.ru>
2019-11-08 13:02:44 +0300

    overlay show and close

--- js/custom-modal.js
+++ js/custom-modal.js
@@ -3,50 +3,123 @@
         var config = $.extend({}, {
             btnOpenModal: '',
             modalBlock: '',
-            speedOpenModal: 400,
+            customClass: '',
+            speedOpenModal: 500,
             btnClose: true,
             overlayClose: true,
+            overlayShow: true,
             modalShow: 'fade'
         }, options);
 
         return this.each(function() {
             let modalBlock = $(options.modalBlock);
+            let modalName = options.modalBlock.slice(1);
             let cloneModalBlock = $(modalBlock).clone();
             $(modalBlock).remove();
+
             $('body').append(
                 '<div class="custom-modal">' +
-                '<div class="custom-modal__overlay jsCustomModalClose"></div>' +
-                '<div class="custom-modal__content">' +
-                '<button class="custom-modal__close jsCustomModalClose"></button>' +
+                '<div class="custom-modal__overlay jsCustomModalCloseOverlay_' + modalName + '"></div>' +
+                '<div class="custom-modal__content ' + options.customClass + ' jsAddContent' + modalName + '">' +
+                '<button class="custom-modal__close jsCustomModalClose_' + modalName + '"></button>' +
                 '</div>' +
                 '</div>'
             );
-            $('.custom-modal__content').append($(cloneModalBlock));
+            $('.jsAddContent' + modalName).append($(cloneModalBlock));
 
-            if(options.btnClose == false) {
-                $('.custom-modal__close').hide();
+            if(options.btnClose === false) {
+                $('.jsCustomModalClose_' + modalName).hide();
             }
 
-            if(options.overlayClose == false) {
-                $('.custom-modal__overlay').hide();
+            if(options.overlayShow === false) {
+                $('.jsCustomModalCloseOverlay_' + modalName).hide();
             }
 
-            // if(options.modalShow == 'fade') {
-            //     console.log(options.modalShow);
-            //
-            //     $('.custom-modal__content')
-            //
-            // }
+            function showOptions() {
+                switch (options.modalShow) {
+                    case 'fade':
+                        $('.jsAddContent' + modalName).css({
+                            'top': '50%',
+                            'left': '50%',
+                            'transform': 'translate(-50%,-50%)'
+                        });
+                        break;
+                    case 'top':
+                        $('.jsAddContent' + modalName).css({
+                            'top': '-50%',
+                            'left': '50%',
+                            'transform': 'translateX(-50%)'
+                        });
+                        break;
+                    case 'right':
+                        $('.jsAddContent' + modalName).css({
+                            'top': '50%',
+                            'right': '-50%',
+                            'transform': 'translateY(-50%)'
+                        });
+                        break;
+                    case 'bottom':
+                        $('.jsAddContent' + modalName).css({
+                            'bottom': '-50%',
+                            'left': '50%',
+                            'transform': 'translateX(-50%)'
+                        });
+                        break;
+                    case 'left':
+                        $('.jsAddContent' + modalName).css({
+                            'top': '50%',
+                            'left': '-50%',
+                            'transform': 'translateX(-50%)'
+                        });
+                        break;
+                }
+            }
+            showOptions();
+
+            function showOpen() {
+                switch (options.modalShow) {
+                    case 'top':
+                        $('.jsAddContent' + modalName).animate({ "top": "50%" }, options.speedOpenModal).css({
+                            'transform': 'translate(-50%,-50%)'
+                        });
+                        break;
+                    case 'right':
+                        $('.jsAddContent' + modalName).animate({ "right": "50%" }, options.speedOpenModal).css({
+                            'transform': 'translate(50%,-50%)'
+                        });
+                        break;
+                    case 'bottom':
+                        $('.jsAddContent' + modalName).animate({ "bottom": "50%" }, options.speedOpenModal).css({
+                            'transform': 'translate(-50%,50%)'
+                        });
+                        break;
+                    case 'left':
+                        $('.jsAddContent' + modalName).animate({ "left": "50%" }, options.speedOpenModal).css({
+                            'transform': 'translate(-50%,-50%)'
+                        });
+                        break;
+                }
+            }
 
             $(options.btnOpenModal).click(function () {
                 $(options.modalBlock).closest($('.custom-modal')).fadeIn(options.speedOpenModal);
                 $('body').css({'overflow': 'hidden'});
+                showOpen();
             });
 
-            $('.jsCustomModalClose').click(function () {
+            $('.jsCustomModalClose_' + modalName).click(function () {
                 $(this).closest('.custom-modal').fadeOut(options.speedOpenModal);
                 $('body').css({'overflow': 'visible'});
+                showOptions();
             });
+
+            if(options.overlayClose === true) {
+                $('.jsCustomModalCloseOverlay_' + modalName).click(function () {
+                    $(this).closest('.custom-modal').fadeOut(options.speedOpenModal);
+                    $('body').css({'overflow': 'visible'});
+                    showOptions();
+                });
+            }
 
         });
     };
--- js/script.js
+++ js/script.js
@@ -3,18 +3,22 @@
     $(".jsOpenModal").customModal({
         btnOpenModal: '.jsOpenModal',
         modalBlock: '.jsModal',
-        speedOpenModal: 400,
+        customClass: '',
+        speedOpenModal: 500,
         btnClose: true,
         overlayClose: true,
-        modalShow: 'top' // fade (default), top, right, bottom, left
+        overlayShow: true,
+        modalShow: 'fade' // fade (default), top, right, bottom, left
     });
 
     $(".jsOpenModall").customModal({
         btnOpenModal: '.jsOpenModall',
         modalBlock: '.jsModall',
-        speedOpenModal: 400,
+        customClass: 'custom-class',
+        speedOpenModal: 500,
         btnClose: true,
-        overlayClose: true,
+        overlayClose: false,
+        overlayShow: true,
         modalShow: 'top' // fade (default), top, right, bottom, left
     });
 
